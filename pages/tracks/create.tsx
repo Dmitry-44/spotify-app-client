506f4033c086c89759fef45c804bf70906ad021e
Dmitry Sakovich <sakovich.dima95@gmail.com>
2022-04-20 18:31:54 +0300

    loading track card starting

--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
+import StepWrapper from '../../components/StepWrapper';
+import MainLayout from '../../layouts/MainLayout';
+import {Grid,Button} from '@mui/material';
 
 const create = () => {
+    const [activeStep, setActiveStep] = useState(0)
+
+    const next = () => {
+        setActiveStep( prev => prev + 1)
+    }
+    const prev = () => {
+        setActiveStep( prev => prev - 1)
+    }
     return (
-        <div>
-            creeeate
-        </div>
+        <MainLayout>
+            <StepWrapper activeStep={activeStep}>
+                <h1>Загрузка трека</h1>
+            </StepWrapper>
+            <Grid container justifyContent='space-between'>
+                <Button onClick={prev}>Назад</Button>
+                <Button onClick={next}>Вперед</Button>
+            </Grid>
+        </MainLayout>
     );
 };
 
