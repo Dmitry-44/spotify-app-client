93d1c30aac90d2d07de86f1e3e7fe4c5a9d885b2
Dmitry Sakovich <sakovich.dima95@gmail.com>
2022-05-18 18:25:43 +0300

    add player

---
+++ components/Player.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {IconButton, Grid} from '@mui/material';
+import { Pause, PlayArrow } from '@mui/icons-material';
+import styles from '../styles/Player.module.sass';
+import {ITrack} from '../types/track'
+import TrackProgress from './TrackProgress';
+
+const Player = () => {
+    const active=false;
+    const track: ITrack = {_id: '1', name: 'MOW', artist: 'Gunna', text: 'moowowo', listens: 40, audio:'', picture:'https://upload.wikimedia.org/wikipedia/ru/5/53/Wunna_%28Deluxe%29.png', comments: []};
+    return (
+        <div className={styles.player}>
+            <IconButton onClick={e => e.stopPropagation()}>
+                {active 
+                    ? <Pause />
+                    : <PlayArrow />
+                }
+            </IconButton>
+            <Grid container direction='column' style={{margin: '0 20px', width: 200}}>
+                <div>{track.name}</div>
+                <div style={{fontSize:12, color: 'grey'}}>{track.artist}</div>
+            </Grid>
+            <TrackProgress left={0} right={100} onChange={()=> {}} />
+        </div>
+    );
+};
+
+export default Player;
--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@mui/material';
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Player from '../components/Player'
 
 const MainLayout = ({children}) => {
     return (
@@ -9,6 +10,7 @@ const MainLayout = ({children}) => {
             <Container style={{margin:'90px auto'}}>
                 {children}
             </Container>
+            <Player/>
         </>
     );
 };
