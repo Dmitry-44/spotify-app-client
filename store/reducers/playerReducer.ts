266e19ece83b609a6735474b30434acb36d8891a
Dmitry Sakovich <sakovich.dima95@gmail.com>
2022-06-22 18:11:42 +0300

    add reducer

---
+++ store/reducers/playerReducer.ts
@@ -0,0 +1,29 @@
+import { PlayerAction, PlayerState, PlayerActionTypes } from "../../types/player"
+
+const initialState: PlayerState= {
+    active: null,
+    pause: true,
+    duration: 0,
+    currentTime: 0,
+    volume: 0
+}
+
+export const playerReducer = (state=initialState,action: PlayerAction): PlayerState => {
+    switch (action.type) {
+
+        case PlayerActionTypes.PAUSE:
+            return {...state, pause:true}
+        case PlayerActionTypes.PLAY:
+            return {...state,pause:false}
+        case PlayerActionTypes.SET_ACTIVE:
+            return {...state,active: action.payload, currentTime:0,duration:0}
+        case PlayerActionTypes.SET_DURATION:
+            return {...state, duration: action.payload}
+        case PlayerActionTypes.SET_CURRENT_TIME:
+            return {...state, currentTime: action.payload}
+        case PlayerActionTypes.SET_VOLUME:
+            return {...state, volume: action.payload}
+
+        default: return state
+    }
+}
